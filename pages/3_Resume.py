54426a2fe5b8a291f4b4b02da5631e14e9733d4a
manavgarkel <manav.garkel@gmail.com>
2025-04-14 14:38:55 -0400

    added new resume

--- pages/3_Resume.py
+++ pages/3_Resume.py
@@ -6,7 +6,7 @@ local_css("style/style.css")
 render_sidebar_photo()
 
 st.title("📝 Resume")
-with open("images/Manav_Garkel_Resume.pdf", "rb") as f:
+with open("images/Manav Garkel Resume.pdf", "rb") as f:
     base64_pdf = base64.b64encode(f.read()).decode("utf-8")
     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000mm" height="1000mm" type="application/pdf"></iframe>'
     st.markdown(pdf_display, unsafe_allow_html=True)
